1267b508e10da13d10edeb6d93bdc3e967799d09
nguyenhy <nguyenhy311@gmail.com>
2026-06-23 11:33:20 +0700

    chore: add logger utils

---
+++ src/lib/logger.ts
@@ -0,0 +1,28 @@
+export const createLogger = () => {
+  const uid = Math.floor(Math.random() * 10000000) + Date.now();
+
+  const infoLogger = (...args: unknown[]) => {
+    console.log(
+      //
+      "[INF]",
+      new Date().toISOString(),
+      uid,
+      ...args,
+    );
+  };
+
+  const errorLogger = (...args: unknown[]) => {
+    console.error(
+      //
+      "[ERR]",
+      new Date().toISOString(),
+      uid,
+      ...args,
+    );
+  };
+
+  return {
+    info: infoLogger,
+    error: errorLogger,
+  };
+};
