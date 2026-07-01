04ff4b4354a9127269597c28c3b2509be7ce9a42
nguyenhy <nguyenhy311@gmail.com>
2026-07-01 07:09:44 +0700

    feat(sitemap): update last modified

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
-import { fetchFromBff } from "@/lib/fetch";
+// import { fetchFromBff } from "@/lib/fetch";
 import { createLogger } from "@/lib/logger";
-import { getRawKey, isObject } from "@/lib/validate/extract";
+// import { getRawKey, isObject } from "@/lib/validate/extract";
 import type { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
@@ -8,60 +8,58 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   logger.info(["sitemap"]);
 
   const templates: MetadataRoute.Sitemap = [];
-  try {
-    const search = new URLSearchParams();
-    search.set(
-      "fields",
-      [
-        //
-        "slug",
-        "date_updated",
-        "product.date_updated",
-      ].join(","),
-    );
-    search.set("filter[archived][_neq]", "true");
+  // try {
+  //   const search = new URLSearchParams();
+  //   search.set(
+  //     "fields",
+  //     [
+  //       //
+  //       "slug",
+  //       "date_updated",
+  //       "product.date_updated",
+  //     ].join(","),
+  //   );
+  //   search.set("filter[archived][_neq]", "true");
 
-    const response = await fetchFromBff(`/items/template?${search}`);
-    if (response.ok) {
-      const json = await response.json();
-      const raw = !!json && isObject(json) ? json : null;
-      const rawData = !!raw && getRawKey(raw, "data");
-      const data = !!rawData && Array.isArray(rawData) ? rawData : [];
+  //   const response = await fetchFromBff(`/items/template?${search}`);
+  //   if (response.ok) {
+  //     const json = await response.json();
+  //     const raw = !!json && isObject(json) ? json : null;
+  //     const rawData = !!raw && getRawKey(raw, "data");
+  //     const data = !!rawData && Array.isArray(rawData) ? rawData : [];
 
-      logger.info(["sitemap", data.length]);
-
-      for (let index = 0; index < data.length; index++) {
-        const item = data[index];
-        const templateUpdatedAt = new Date(item.date_updated);
-        const productUpdatedAt = new Date(item.product.date_updated);
-        const updateAt = Math.max(
-          productUpdatedAt.valueOf(),
-          templateUpdatedAt.valueOf(),
-        );
-        const lastModified = isNaN(updateAt) ? new Date() : new Date(updateAt);
-        const [route]: MetadataRoute.Sitemap = [
-          {
-            url: `https://simplakit.com/template/${item.slug}`,
-            lastModified: lastModified,
-            changeFrequency: "weekly",
-            priority: 0.9,
-          },
-        ];
-        templates.push(route);
-      }
-    } else {
-      logger.info(["fetch.failed", response.ok, await response.text()]);
-    }
-  } catch (error) {
-    logger.info(["fetch.error", String(error)]);
-  }
+  //     for (let index = 0; index < data.length; index++) {
+  //       const item = data[index];
+  //       const templateUpdatedAt = new Date(item.date_updated);
+  //       const productUpdatedAt = new Date(item.product.date_updated);
+  //       const updateAt = Math.max(
+  //         productUpdatedAt.valueOf(),
+  //         templateUpdatedAt.valueOf(),
+  //       );
+  //       const lastModified = isNaN(updateAt) ? new Date() : new Date(updateAt);
+  //       const [route]: MetadataRoute.Sitemap = [
+  //         {
+  //           url: `https://simplakit.com/template/${item.slug}`,
+  //           lastModified: lastModified,
+  //           changeFrequency: "weekly",
+  //           priority: 0.9,
+  //         },
+  //       ];
+  //       templates.push(route);
+  //     }
+  //   } else {
+  //     logger.info(["fetch.failed", response.ok, await response.text()]);
+  //   }
+  // } catch (error) {
+  //   logger.info(["fetch.error", String(error)]);
+  // }
 
   return [
     // home
     {
       url: "https://simplakit.com",
       lastModified: new Date(
-        "Mon Jun 29 2026 11:54:59 GMT+0700 (Indochina Time)",
+        "Mon Jul 01 2026 07:08:25 GMT+0700 (Indochina Time)",
       ),
       changeFrequency: "monthly",
       priority: 1,
@@ -69,20 +67,20 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
     // product
     ...templates,
-    {
-      url: "https://simplakit.com/templates",
-      lastModified: new Date(
-        "Mon Jun 29 2026 11:54:59 GMT+0700 (Indochina Time)",
-      ),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
+    // {
+    //   url: "https://simplakit.com/templates",
+    //   lastModified: new Date(
+    //     "Mon Jul 01 2026 07:08:25 GMT+0700 (Indochina Time)",
+    //   ),
+    //   changeFrequency: "weekly",
+    //   priority: 0.8,
+    // },
 
     // contact
     {
       url: "https://simplakit.com/contact",
       lastModified: new Date(
-        "Mon Jun 29 2026 11:54:59 GMT+0700 (Indochina Time)",
+        "Mon Jul 01 2026 07:08:25 GMT+0700 (Indochina Time)",
       ),
       priority: 0.3,
     },
@@ -91,21 +89,21 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: "https://simplakit.com/terms",
       lastModified: new Date(
-        "Mon Jun 29 2026 11:54:59 GMT+0700 (Indochina Time)",
+        "Mon Jul 01 2026 07:08:25 GMT+0700 (Indochina Time)",
       ),
       priority: 0.5,
     },
     {
       url: "https://simplakit.com/privacy",
       lastModified: new Date(
-        "Mon Jun 29 2026 11:54:59 GMT+0700 (Indochina Time)",
+        "Mon Jul 01 2026 07:08:25 GMT+0700 (Indochina Time)",
       ),
       priority: 0.5,
     },
     {
       url: "https://simplakit.com/refund",
       lastModified: new Date(
-        "Mon Jun 29 2026 11:54:59 GMT+0700 (Indochina Time)",
+        "Mon Jul 01 2026 07:08:25 GMT+0700 (Indochina Time)",
       ),
       priority: 0.5,
     },
